e894e6c6e0c80a34fbc73f6190334f6613b9c3fd
Gabriel Kassarp <gkassarp@gmail.com>
2025-05-29 11:20:36 +0200

    added assets and types

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,9 @@ import './globals.css'
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <Stack>
+  <Stack.Screen name="(tabs)" options={{ headerShown: false }} key="root" />
+  <Stack.Screen name="movies/[id]" options={{ headerShown: false }} key="root" />
+  </Stack>)
 }
